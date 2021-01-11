831a267fd074473d26b291bd9dc7f4799cf370ad
japc <japc.grafico@gmail.com>
2021-01-11 16:40:11 +0100

    Login Form validation in the template

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from '../../models/user.models';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user: UserModel = new UserModel();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(form: NgForm): void {
+    if (form.invalid) { return; }
+
+    console.log('Form send');
+    console.log(this.user);
+    console.log(form);
   }
 
 }
