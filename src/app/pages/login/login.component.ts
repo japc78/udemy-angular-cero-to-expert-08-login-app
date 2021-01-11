e2d2f902faa818760290decc396e3d577b366ec6
japc <japc.grafico@gmail.com>
2021-01-11 18:37:56 +0100

    Show message with SweetAlert2

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserModel } from '../../models/user.models';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -20,11 +21,25 @@ export class LoginComponent implements OnInit {
   login(form: NgForm) {
     if (form.invalid) { return; }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Wait please...'
+    });
+
+    Swal.showLoading();
+
     return this.auth.login(this.user)
       .subscribe(resp => {
         console.log(resp);
+        Swal.close();
       }, (err) => {
-        console.log(err.error.error.message);
+        // console.log(err.error.error.message);
+        Swal.fire({
+          allowOutsideClick: true,
+          icon: 'error',
+          text: err.error.error.message
+        });
       });
   }
 }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserModel } from '../../models/user.models';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -23,11 +24,25 @@ export class RegistroComponent implements OnInit {
   onSubmit(form: NgForm): void {
     if (form.invalid) { return; }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Wait please...',
+    });
+
+    Swal.showLoading();
+
     this.auth.newUser(this.user)
       .subscribe( resp => {
         console.log(resp);
+        Swal.close();
       }, (err) => {
         console.log(err.error.error.message);
+        Swal.fire({
+          allowOutsideClick: true,
+          icon: 'error',
+          text: err.error.error.message
+        });
       });
 
     console.log('Form send');
